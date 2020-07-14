771f7102257ffa8444f188328f2389fc7d526df5
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-14 17:09:32 +0300

    Add basic API with JSON response of anonymous objects list

---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    //[Route("api/[controller]")] // same as above as cities is the name of the controller
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<Object>()
+                {
+                new { id = 1, Name = "New York City"},
+                new { id = 2, Name = "Antwerp"}
+            });
+        }
+    }
+}
--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -16,6 +16,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,15 +31,9 @@ namespace CityInfo.API
                 app.UseExceptionHandler();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseRouting();
+            app.UseMvc();
+            
         }
     }
 }
