b1b1d60b10b2ae3142d13050da4ddc30e0f018d5
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-14 18:27:21 +0300

    Add accept/xml next to json output

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,7 +17,22 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddMvc(option => option.EnableEndpointRouting = false)
+                .AddMvcOptions( o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    //o.OutputFormatters.Remove(Json thing);
+                    //o.OutputFormatters.Clear();
+                });
+            // there is a way to adapt to old Json info - working with serializer settings - variable names begin with Caps
+            // .AddJsonOptions( o =>
+            //{
+            //  if (o.SerializerSettings.ContractResolver != null)
+            //  {
+            //      var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+            //      castedResolver.NamingStrategy = null;
+            //  }
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
